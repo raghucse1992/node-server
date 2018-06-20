8a8bc16f3e577fc30dabc09411ef4f5806cfeb4f
raghavendra.mogera <raghavendra.mogera@robosoftin.com>
2018-06-20 12:52:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs  = require('fs');
+
+var app = express();
+
+app.set('view engin', 'hbs');
+app.use(express.static(__dirname+ '/public'));
+
+app.use((req,res,next)=>{
+     var now = new Date().toString();
+     var log = `${now} : ${req.method}  ${req.url}`;
+     fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log('Unable to append file');
+        }
+     })
+     next();
+});
+app.get('/',(req,res) => {
+    //res.send('Hello Express');
+    res.send({
+        name:'Raghu',
+        likes:[
+            'Biking',
+            'Cities'
+        ]
+    });
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+        currentYear:new Date().getFullYear()
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
